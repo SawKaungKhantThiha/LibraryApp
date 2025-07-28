2787bf18457237b346dc8d95c34f08df8151fa6a
SawKaungKhantThiha <sawkaungkhantthiha@gmail.com>
2025-07-28 23:35:13 +0800

    final

--- app.js
+++ app.js
@@ -797,4 +797,7 @@ app.get('/userLoans', checkAuthenticated, (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
